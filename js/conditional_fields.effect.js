9e8af8fc814410c108ad2c2f979bfd87aae0ff9a
peterpoe <gmagini@gmail.com>
2011-08-11 19:41:44 +0200

    Use Javascript API behaviors in conditional_fields.effect.js

--- js/conditional_fields.effect.js
+++ js/conditional_fields.effect.js
@@ -4,28 +4,32 @@
  * Override states.js "state:visible" event for fields with custom effects.
  */
 {
-  $(document).ready(function() {
-    $.each(Drupal.settings.conditionalFields.effects, function(dependent) {
-      $('[name="'+dependent+'"]').unbind('state:visible').bind('state:visible', function(e) {
-        if (e.trigger) {
-          var effect = Drupal.settings.conditionalFields.effects[$(e.target).attr('name')];
-          switch (effect.effect) {
-            case 'fade':
-              $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut'](parseInt(effect.options.speed));
-              break;
-            case 'slide':
-              $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'slideDown' : 'slideUp'](parseInt(effect.options.speed));
-              break;
-            default:
-              // The "effect" variable is treated as a jQuery plugin.
-              $(e.target)[effect.effect](e);
+
+  Drupal.behaviors.conditionalFields = {
+    attach: function (context, settings) {
+      $.each(settings.conditionalFields.effects, function(dependent) {
+        $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
+          if (e.trigger) {
+            var effect = Drupal.settings.conditionalFields.effects[$(e.target).attr('name')];
+            switch (effect.effect) {
+              case 'fade':              
+                $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut'](parseInt(effect.options.speed));
+                break;
+              case 'slide':
+                $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'slideDown' : 'slideUp'](parseInt(effect.options.speed));
+                break;
+              default:
+                // The "effect" variable is treated as a jQuery plugin.
+                $(e.target)[effect.effect](e);
+            }
           }
-        }
-        // Prevent bubbling of the event to document.
-        return false;
+          // Prevent bubbling of the event to document.
+          return false;
+        });
       });
-    });
-  });
+    }
+  };
+
 }
 
 })(jQuery);
