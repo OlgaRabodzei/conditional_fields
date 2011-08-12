80d550d577f6ea202ad4c15a4c5b80795ed8e931
peterpoe <gmagini@gmail.com>
2011-08-13 01:00:28 +0200

    Enhance effects system and add two new States: Filled with value / Emptied.

--- js/conditional_fields.effect.js
+++ js/conditional_fields.effect.js
@@ -8,24 +8,50 @@
   Drupal.behaviors.conditionalFields = {
     attach: function (context, settings) {
       $.each(settings.conditionalFields.effects, function(dependent) {
-        $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
-          if (e.trigger) {
-            var effect = Drupal.settings.conditionalFields.effects[$(e.target).attr('name')];
-            switch (effect.effect) {
-              case 'fade':              
+        var effect = settings.conditionalFields.effects[dependent];
+        switch (effect.effect) {
+          case 'fade':
+            $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
+              if (e.trigger) {
                 $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut'](parseInt(effect.options.speed));
-                break;
-              case 'slide':
+              }
+              // Prevent bubbling of the event to document.
+              return false;
+            });
+            break;
+
+          case 'slide':
+            $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
+              if (e.trigger) {
                 $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'slideDown' : 'slideUp'](parseInt(effect.options.speed));
-                break;
-              default:
-                // The "effect" variable is treated as a jQuery plugin.
-                $(e.target)[effect.effect](e);
-            }
-          }
-          // Prevent bubbling of the event to document.
-          return false;
-        });
+              }
+              return false;
+            });
+            break;
+
+          case 'fill':
+          case 'empty':
+            $('[name="'+dependent+'"]', context).bind('state:empty', function(e) {
+              if (e.trigger) {
+                if (effect.options.reset) {
+                  if (typeof oldValue == 'undefined' || $(e.target).val() != effect.options.value) {
+                    oldValue = $(e.target).val();
+                  }
+                  $(e.target).val((effect.effect == 'fill' ? e.value : !e.value) ? oldValue : effect.options.value);
+                }
+                else {
+                  if (effect.effect == 'fill' && !e.value || effect.effect == 'empty' && e.value) {
+                    $(e.target).val(effect.options.value);
+                  }
+                }
+              }
+            });
+            break;
+
+          default:
+            // The "effect" variable is treated as a jQuery plugin.
+            $(e.target)[effect.effect](e, effect.options);
+        }
       });
     }
   };
