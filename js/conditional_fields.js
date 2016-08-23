f845598760e3bf5d52c27f813b9c8218a917b84e
Dmitry Polkhov <dpolkhov@adyax.com>
2016-08-23 11:55:02 +0300

    Added functionality for empti and filled states

--- js/conditional_fields.js
+++ js/conditional_fields.js
@@ -1,66 +1,66 @@
 (function ($) {
 
-/**
- * Enhancements to states.js.
- */
-
-/**
- * Handle array values.
- * @see http://drupal.org/node/1149078
- */
-Drupal.states.Dependent.comparisons['Array'] = function (reference, value) {
-  // Make sure value is an array.
-  if (!(typeof(value) === 'object' && (value instanceof Array))) {
-    return false;
-  }
-  // We iterate through each value provided in the reference. If all of them
-  // exist in value array, we return true. Otherwise return false.
-  for (var key in reference) {
-    if (reference.hasOwnProperty(key) && $.inArray(reference[key], value) < 0) {
-      return false;
-    }
-  }
-  return true;
-};
-
-/**
- * Handle Object values.
- */
-Drupal.states.Dependent.comparisons['Object'] = function (reference, value) {
-  /* Regular expressions are objects with a RegExp property. */
-  if (reference.hasOwnProperty('RegExp')) {
-    reference = new RegExp(reference.RegExp);
-    return this.RegExp(reference, value);
-  }
-  else {
-    return compare(reference, value);
-  }
-};
-
-/**
- * Focused condition.
- */
-Drupal.states.Trigger.states.focused = function(element) {
-  element.bind('focus', function () {
-    element.trigger({ type: 'state:focused', value: true });
-  })
-  .bind('blur', function () {
-    element.trigger({ type: 'state:focused', value: false });
-  });
-
-  Drupal.states.postponed.push($.proxy(function () {
-    element.trigger({ type: 'state:focused', value: element.is(':focus') });
-  }, window));
-};
-
-/**
- * Touched condition.
- */
-Drupal.states.Trigger.states.touched = {
-  'focus': function(e) {
-    return (typeof e === 'undefined' && !this.is(':focus')) ? false : true;
-  }
-};
+// /**
+//  * Enhancements to states.js.
+//  */
+//
+// /**
+//  * Handle array values.
+//  * @see http://drupal.org/node/1149078
+//  */
+// Drupal.states.Dependent.comparisons['Array'] = function (reference, value) {
+//   // Make sure value is an array.
+//   if (!(typeof(value) === 'object' && (value instanceof Array))) {
+//     return false;
+//   }
+//   // We iterate through each value provided in the reference. If all of them
+//   // exist in value array, we return true. Otherwise return false.
+//   for (var key in reference) {
+//     if (reference.hasOwnProperty(key) && $.inArray(reference[key], value) < 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+//
+// /**
+//  * Handle Object values.
+//  */
+// Drupal.states.Dependent.comparisons['Object'] = function (reference, value) {
+//   /* Regular expressions are objects with a RegExp property. */
+//   if (reference.hasOwnProperty('RegExp')) {
+//     reference = new RegExp(reference.RegExp);
+//     return this.RegExp(reference, value);
+//   }
+//   else {
+//     return compare(reference, value);
+//   }
+// };
+//
+// /**
+//  * Focused condition.
+//  */
+// Drupal.states.Trigger.states.focused = function(element) {
+//   element.bind('focus', function () {
+//     element.trigger({ type: 'state:focused', value: true });
+//   })
+//   .bind('blur', function () {
+//     element.trigger({ type: 'state:focused', value: false });
+//   });
+//
+//   Drupal.states.postponed.push($.proxy(function () {
+//     element.trigger({ type: 'state:focused', value: element.is(':focus') });
+//   }, window));
+// };
+//
+// /**
+//  * Touched condition.
+//  */
+// Drupal.states.Trigger.states.touched = {
+//   'focus': function(e) {
+//     return (typeof e === 'undefined' && !this.is(':focus')) ? false : true;
+//   }
+// };
 
 /**
  * New and existing states enhanced with configurable options.
@@ -68,7 +68,7 @@ Drupal.states.Trigger.states.touched = {
  * state:stateName-effectName.
  */
 
-// Visible/Invisible.
+//Visible/Invisible.
 $(document).bind('state:visible-fade', function(e) {
   if (e.trigger) {
     $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut'](e.effect.speed);
@@ -114,7 +114,7 @@ $(document).bind('state:visible-fade', function(e) {
 Drupal.behaviors.conditionalFields = {
   attach: function (context, settings) {
     // AJAX is not updating settings.conditionalFields correctly.
-    var conditionalFields = settings.conditionalFields || Drupal.settings.conditionalFields;
+    var conditionalFields = settings.conditionalFields || 'undefined';
     if (typeof conditionalFields === 'undefined' || typeof conditionalFields.effects === 'undefined') {
       return;
     }
