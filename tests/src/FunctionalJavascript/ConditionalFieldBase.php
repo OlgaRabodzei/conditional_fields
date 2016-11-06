d33a2951b204b2ff2eb89b1b9c2b0f6c6d1a515c
Ivan Tsekhmistro <itsekhmistro@adyax.com>
2016-11-06 03:38:16 +0200

    Check State isFilled with JavascriptTestBase. Moved all BrowserTests to JavascriptTests

---
+++ tests/src/FunctionalJavascript/ConditionalFieldBase.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\Tests\conditional_fields\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+
+/**
+ * Base setup for ConditionalField tests.
+ *
+ * @group conditional_fields
+ */
+abstract class ConditionalFieldBase extends JavascriptTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'conditional_fields',
+    'node',
+    'datetime',
+    'field_ui',
+    'taxonomy',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create Basic page and Article node types.
+    if ($this->profile != 'standard') {
+      $this->drupalCreateContentType(array(
+        'type' => 'page',
+        'name' => 'Basic page',
+        'display_submitted' => FALSE,
+      ));
+      $this->drupalCreateContentType(array('type' => 'article', 'name' => 'Article'));
+    }
+    $this->accessHandler = \Drupal::entityManager()->getAccessControlHandler('node');
+  }
+
+  /**
+   * Waits and asserts that a given element is visible.
+   *
+   * @param string $selector
+   *   The CSS selector.
+   * @param int $timeout
+   *   (Optional) Timeout in milliseconds, defaults to 1000.
+   * @param string $message
+   *   (Optional) Message to pass to assertJsCondition().
+   */
+  protected function waitUntilVisible($selector, $timeout = 1000, $message = '') {
+    $condition = "jQuery('" . $selector . ":visible').length > 0";
+    $this->assertJsCondition($condition, $timeout, $message);
+  }
+
+  /**
+   * Waits and asserts that a given element is hidden (invisible).
+   *
+   * @param string $selector
+   *   The CSS selector.
+   * @param int $timeout
+   *   (Optional) Timeout in milliseconds, defaults to 1000.
+   * @param string $message
+   *   (Optional) Message to pass to assertJsCondition().
+   */
+  protected function waitUntilHidden($selector, $timeout = 1000, $message = '') {
+    $condition = "jQuery('" . $selector . ":hidden').length > 0";
+    $this->assertJsCondition($condition, $timeout, $message);
+  }
+
+  /**
+   * Helper to change Field value with Javascript.
+   */
+  protected function changeField($selector, $value = '') {
+    $this->getSession()->executeScript("jQuery('" . $selector . "').val('" . $value . "').trigger('keyup').trigger('change');");
+  }
+
+}
--- tests/src/FunctionalJavascript/ConditionalFieldStatesTest.php
+++ tests/src/FunctionalJavascript/ConditionalFieldStatesTest.php
@@ -2,8 +2,7 @@
 
 namespace Drupal\Tests\conditional_fields\FunctionalJavascript;
 
-use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
-use Drupal\Tests\video_embed_field\Functional\EntityDisplaySetupTrait;
+use Drupal\Tests\conditional_fields\FunctionalJavascript\ConditionalFieldBase as JavascriptTestBase;
 
 /**
  * Test the colorbox formatter.
@@ -12,56 +11,10 @@ use Drupal\Tests\video_embed_field\Functional\EntityDisplaySetupTrait;
  */
 class ConditionalFieldStatesTest extends JavascriptTestBase {
 
-  use EntityDisplaySetupTrait;
-
   /**
-   * {@inheritdoc}
-   */
-  public static $modules = [
-    'conditional_fields',
-    'node',
-    'datetime',
-    'field_ui',
-    'taxonomy',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-
-    // Create Basic page and Article node types.
-    if ($this->profile != 'standard') {
-      $this->drupalCreateContentType(array(
-        'type' => 'page',
-        'name' => 'Basic page',
-        'display_submitted' => FALSE,
-      ));
-      $this->drupalCreateContentType(array('type' => 'article', 'name' => 'Article'));
-    }
-    $this->accessHandler = \Drupal::entityManager()->getAccessControlHandler('node');
-
-    // $this->setupEntityDisplays();
-  }
-
-  /**
-   * Tests creating Conditional Field Config.
+   * Tests creating Conditional Field: Visible if isFilled.
    */
   public function testCreateConfig() {
-    // $this->setDisplayComponentSettings('video_embed_field_colorbox', [
-    //   'autoplay' => FALSE,
-    //   'responsive' => TRUE,
-    // ]);
-    // $node = $this->createVideoNode('https://example.com/mock_video');
-    // $this->drupalGet('node/' . $node->id());
-    // $this->click('.video-embed-field-launch-modal');
-    // $this->getSession()->wait(static::COLORBOX_LAUNCH_TIME);
-    // $this->assertSession()->elementExists('css', '#colorbox .video-embed-field-responsive-video');
-    // // Make sure the right library files are loaded on the page.
-    // $this->assertSession()->elementContains('css', 'style', 'colorbox/styles/default/colorbox_style.css');
-    // $this->assertSession()->elementContains('css', 'style', 'video_embed_field/css/video_embed_field.responsive-video.css');
-
     $admin_account = $this->drupalCreateUser([
       'view conditional fields',
       'edit conditional fields',
@@ -105,55 +58,15 @@ class ConditionalFieldStatesTest extends JavascriptTestBase {
     // Visit Article Add form to check that conditions are applied.
     $this->drupalGet('node/add/article');
     $this->assertSession()->statusCodeEquals(200);
-    $this->createScreenshot('screenshot.jpg');
-    $this->assertFileExists('screenshot.jpg');
 
     // Check that the field Body is not visible.
-    // $a = $this->cssSelectToXpath('.field--name-body');
-    // $this->assertElementNotVisible('.field--name-body', 'Article Body field is not visible');
+    // $this->createScreenshot('screenshot.jpg');
+    // $this->assertFileExists('screenshot.jpg');
+    // $this->assertElementNotVisible('.field--name-body',
+    // 'Article Body field is not visible');
     $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
-    $this->changeField('.field--name-title input', 'Not empty Title');
-    $this->waitUntilHidden('.field--name-body', 100, 'Article Body field is not visible');
-    $this->createScreenshot('screenshot_visible.jpg');
-    $this->assertFileExists('screenshot_visible.jpg');
-
-  }
-
-  /**
-   * Waits and asserts that a given element is visible.
-   *
-   * @param string $selector
-   *   The CSS selector.
-   * @param int $timeout
-   *   (Optional) Timeout in milliseconds, defaults to 1000.
-   * @param string $message
-   *   (Optional) Message to pass to assertJsCondition().
-   */
-  protected function waitUntilVisible($selector, $timeout = 1000, $message = '') {
-    $condition = "jQuery('" . $selector . ":visible').length > 0";
-    $this->assertJsCondition($condition, $timeout, $message);
-  }
-
-  /**
-   * Waits and asserts that a given element is hidden (invisible).
-   *
-   * @param string $selector
-   *   The CSS selector.
-   * @param int $timeout
-   *   (Optional) Timeout in milliseconds, defaults to 1000.
-   * @param string $message
-   *   (Optional) Message to pass to assertJsCondition().
-   */
-  protected function waitUntilHidden($selector, $timeout = 1000, $message = '') {
-    $condition = "jQuery('" . $selector . ":hidden').length > 0";
-    $this->assertJsCondition($condition, $timeout, $message);
-  }
-
-  /**
-   * 
-   */
-  protected function changeField($selector, $value = '') {
-    $this->getSession()->executeScript("jQuery('" . $selector . "').val('" . $value . "');");
+    $this->changeField('.field--name-title input', 'This field is not empty.');
+    $this->waitUntilVisible('.field--name-body', 10, 'Article Body field is not visible');
   }
 
 }
