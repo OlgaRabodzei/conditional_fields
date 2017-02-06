c197b99e3ac15e940563585d6d2726cc9d2d9ca2
zrashevskaya <zrashevskaya@adyax.com>
2017-02-06 16:11:45 +0200

    Tests for checkbox, radios and text added

--- tests/src/FunctionalJavascript/ConditionalFieldCheckboxesTest.php
+++ tests/src/FunctionalJavascript/ConditionalFieldCheckboxesTest.php
@@ -33,7 +33,7 @@ class ConditionalFieldCheckboxesTest extends JavascriptTestBase {
   /**
    * Tests creating Conditional Field: Visible if has value from taxonomy.
    */
-  public function testCreateConfig() {
+  public function testCreateConfigVisibleValueAnd() {
     $user = $this->drupalCreateUser([
       'administer nodes',
       'administer content types',
@@ -86,9 +86,73 @@ class ConditionalFieldCheckboxesTest extends JavascriptTestBase {
     $this->waitUntilVisible('.field--name-body', 60, 'Article Body field is not visible');
     // Change a select value set to hide the body again.
     $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_1, $term_id_1);
+    $this->waitUntilHidden('.field--name-body', 60, 'Article Body field is visible');
     $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_2, $term_id_2);
     $this->waitUntilHidden('.field--name-body', 60, 'Article Body field is visible');
     $this->createScreenshot('sites/simpletest/scr2BodyHidCheckboxes.jpg');
+  }
+
+  /**
+   * Tests creating Conditional Field: Visible if has value from taxonomy.
+   */
+  public function testCreateConfigVisibleValueOr() {
+    $user = $this->drupalCreateUser([
+      'administer nodes',
+      'administer content types',
+      'view conditional fields',
+      'edit conditional fields',
+      'delete conditional fields',
+      'create article content',
+    ]);
+    $this->drupalLogin($user);
+
+    // Visit a ConditionalFields configuration page that requires login.
+    $this->drupalGet('admin/structure/types');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Content` entity type.
+    $this->assertSession()->pageTextContains('Article Dependencies');
+
+    // Visit a ConditionalFields configuration page for Content bundles.
+    $this->createCondition('admin/structure/types/manage/article/conditionals', 'body', 'field_' . $this->taxonomyName, 'visible', 'value' );
+
+    // Change a condition's values set and the value.
+    $this->changeField('#edit-values-set', CONDITIONAL_FIELDS_DEPENDENCY_VALUES_OR);
+    // Random term id to check necessary values.
+    $term_id_1 = mt_rand(1, $this->termsCount);
+    do {$term_id_2 = mt_rand(1, $this->termsCount);}
+    while ($term_id_2 == $term_id_1);
+    $values = $term_id_1.'\r\n'.$term_id_2;
+    $this->changeField('#edit-values', $values);
+
+    // Submit the form.
+    $this->getSession()
+      ->executeScript("jQuery('#conditional-field-edit-form-tab').submit();");
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Check if that configuration is saved.
+    $this->drupalGet('admin/structure/types/manage/article/conditionals');
+    $this->assertSession()
+      ->pageTextContains('body field_' . $this->taxonomyName . ' visible value');
+
+    // Visit Article Add form to check that conditions are applied.
+    $this->drupalGet('node/add/article');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Check that the field Body is not visible.
+    $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
+    // Change a select value set to show the body.
+    $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_1, $term_id_1);
+    $this->waitUntilVisible('.field--name-body', 60, 'Article Body field is not visible');
+    // Change a select value set to hide the body again.
+    $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_1, $term_id_1);
+    $this->waitUntilHidden('.field--name-body', 60, 'Article Body field is visible');
+    // Change a select value set to show the body.
+    $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_2, $term_id_2);
+    $this->waitUntilVisible('.field--name-body', 60, 'Article Body field is not visible');
+    // Change a select value set to hide the body again.
+    $this->changeSelect('#edit-field-' . $this->taxonomyName . '-' . $term_id_2, $term_id_2);
+    $this->waitUntilHidden('.field--name-body', 60, 'Article Body field is visible');
   }
 
   /**
--- tests/src/FunctionalJavascript/ConditionalFieldTextTest.php
+++ tests/src/FunctionalJavascript/ConditionalFieldTextTest.php
@@ -14,7 +14,7 @@ class ConditionalFieldTextTest extends JavascriptTestBase {
   /**
    * Tests creating Conditional Field: Visible if has value from Title value.
    */
-  public function testCreateConfig() {
+  public function testCreateConfigVisibleValueAnd() {
     $user = $this->drupalCreateUser([
       'administer nodes',
       'administer content types',
@@ -57,18 +57,77 @@ class ConditionalFieldTextTest extends JavascriptTestBase {
     // Check that the field Body is not visible.
     $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
     $this->changeField('.field--name-title input', $text);
-//    $this->createScreenshot('sites/simpletest/scr1BodyVis.jpg');
+//    $this->createScreenshot('sites/simpletest/scr1BodyAndVis.jpg');
     $this->waitUntilVisible('.field--name-body', 50, 'Article Body field is not visible');
     // Change a select value set to hide the body again.
     $this->changeField('.field--name-title input', $text . 'a');
     $this->waitUntilHidden('.field--name-body', 50, 'Article Body field is visible');
-//    $this->createScreenshot('sites/simpletest/scr2BodyHid.jpg');
+//    $this->createScreenshot('sites/simpletest/scr2BodyAndHid.jpg');
+  }
+
+  /**
+   * Tests creating Conditional Field: Visible if has value from Title value.
+   */
+  public function testCreateConfigVisibleValueOr() {
+    $user = $this->drupalCreateUser([
+      'administer nodes',
+      'administer content types',
+      'view conditional fields',
+      'edit conditional fields',
+      'delete conditional fields',
+      'create article content',
+    ]);
+    $this->drupalLogin($user);
+
+    // Visit a ConditionalFields configuration page that requires login.
+    $this->drupalGet('admin/structure/types');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Content` entity type.
+    $this->assertSession()->pageTextContains('Article Dependencies');
+
+    // Visit a ConditionalFields configuration page for Content bundles.
+    $this->createCondition('admin/structure/types/manage/article/conditionals', 'body', 'title', 'visible', 'value' );
+
+    // Change a condition's values set and the value.
+    $this->changeField('#edit-values-set', CONDITIONAL_FIELDS_DEPENDENCY_VALUES_OR);
+    // Random term id to check necessary value.
+    $text1 = $this->getRandomGenerator()->word(8);
+    $text2 = $this->getRandomGenerator()->word(7);
+    $values = $text1.'\r\n'.$text2;
+    $this->changeField('#edit-values', $values);
+    // Submit the form.
+    $this->getSession()
+      ->executeScript("jQuery('#conditional-field-edit-form-tab').submit();");
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Check if that configuration is saved.
+    $this->drupalGet('admin/structure/types/manage/article/conditionals');
+    $this->assertSession()
+      ->pageTextContains('body title visible value');
+
+    // Visit Article Add form to check that conditions are applied.
+    $this->drupalGet('node/add/article');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Check that the field Body is not visible.
+    $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
+    $this->changeField('.field--name-title input', $text1);
+    $this->waitUntilVisible('.field--name-body', 50, 'Article Body field is not visible');
+    // Change a select value set to hide the body again.
+    $this->changeField('.field--name-title input', $text1 . 'a');
+    $this->waitUntilHidden('.field--name-body', 50, 'Article Body field is visible');
+    $this->changeField('.field--name-title input', $text2);
+    $this->waitUntilVisible('.field--name-body', 50, 'Article Body field is not visible');
+    // Change a select value set to hide the body again.
+    $this->changeField('.field--name-title input', $text2 . 'ma');
+    $this->waitUntilHidden('.field--name-body', 50, 'Article Body field is visible');
   }
 
   /**
    * Tests creating Conditional Field: Visible if has value from Title Widget.
    */
-  public function testCreateConfigWidget() {
+  public function testCreateConfigVisibleValueWidget() {
     $user = $this->drupalCreateUser([
       'administer nodes',
       'administer content types',
@@ -111,11 +170,102 @@ class ConditionalFieldTextTest extends JavascriptTestBase {
     // Check that the field Body is not visible.
     $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
     $this->changeField('.field--name-title input', $text);
-//    $this->createScreenshot('sites/simpletest/scr1BodyVis.jpg');
     $this->waitUntilVisible('.field--name-body', 50, 'Article Body field is not visible');
     // Change a select value set to hide the body again.
     $this->changeField('.field--name-title input', $text . 'a');
     $this->waitUntilHidden('.field--name-body', 50, 'Article Body field is visible');
-//    $this->createScreenshot('sites/simpletest/scr2BodyHid.jpg');
   }
+
+
+  /**
+   * Tests creating Conditional Field: Visible if isFilled.
+   */
+  public function testCreateConfigVisibleFilled() {
+    $admin_account = $this->drupalCreateUser([
+      'view conditional fields',
+      'edit conditional fields',
+      'delete conditional fields',
+      'administer nodes',
+      'create article content',
+    ]);
+    $this->drupalLogin($admin_account);
+
+    // Visit a ConditionalFields configuration page that requires login.
+    $this->drupalGet('admin/structure/conditional_fields');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Content` entity type.
+    $this->assertSession()->pageTextContains('Content');
+
+    // Visit a ConditionalFields configuration page for Content bundles.
+    $this->drupalGet('admin/structure/conditional_fields/node');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Article` bundle of Content entity type.
+    $this->assertSession()->pageTextContains('Article');
+
+    // Visit a ConditionalFields configuration page for `Article` Content type.
+    $this->createCondition('admin/structure/conditional_fields/node/article', 'body', 'title', 'visible', '!empty');
+
+    // Check that configuration is saved.
+    $this->drupalGet('admin/structure/conditional_fields/node/article');
+    $this->assertSession()->pageTextContains('body title visible !empty');
+
+    // Visit Article Add form to check that conditions are applied.
+    $this->drupalGet('node/add/article');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Check that the field Body is not visible.
+    // $this->createScreenshot('screenshot.jpg');
+    // $this->assertFileExists('screenshot.jpg');
+    // $this->assertElementNotVisible('.field--name-body',
+    // 'Article Body field is not visible');
+    $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
+    $this->changeField('.field--name-title input', 'This field is not empty.');
+    $this->waitUntilVisible('.field--name-body', 10, 'Article Body field is not visible');
+  }
+
+  /**
+   * Tests creating Conditional Field: inVisible if isFilled.
+   */
+  public function testCreateConfigInvisibleEmpty() {
+    $admin_account = $this->drupalCreateUser([
+      'view conditional fields',
+      'edit conditional fields',
+      'delete conditional fields',
+      'administer nodes',
+      'create article content',
+    ]);
+    $this->drupalLogin($admin_account);
+
+    // Visit a ConditionalFields configuration page that requires login.
+    $this->drupalGet('admin/structure/conditional_fields');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Content` entity type.
+    $this->assertSession()->pageTextContains('Content');
+
+    // Visit a ConditionalFields configuration page for Content bundles.
+    $this->drupalGet('admin/structure/conditional_fields/node');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Configuration page contains the `Article` bundle of Content entity type.
+    $this->assertSession()->pageTextContains('Article');
+
+    // Visit a ConditionalFields configuration page for `Article` Content type.
+    $this->createCondition('admin/structure/conditional_fields/node/article', 'body', 'title', '!visible', 'empty');
+
+    // Check that configuration is saved.
+    $this->drupalGet('admin/structure/conditional_fields/node/article');
+    $this->assertSession()->pageTextContains('body title !visible empty');
+
+    // Visit Article Add form to check that conditions are applied.
+    $this->drupalGet('node/add/article');
+    $this->assertSession()->statusCodeEquals(200);
+
+    $this->waitUntilHidden('.field--name-body', 0, 'Article Body field is visible');
+    $this->changeField('.field--name-title input', 'This field is not empty.');
+    $this->waitUntilVisible('.field--name-body', 10, 'Article Body field is not visible');
+  }
+
 }
