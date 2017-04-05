51e43d85a43cf695f615e948136af65b00e8148c
OlgaRabodzei <orabodzei@adyax.com>
2017-04-05 15:49:36 +0300

    Text handler plugin fix

--- src/Plugin/conditional_fields/handler/Text.php
+++ src/Plugin/conditional_fields/handler/Text.php
@@ -24,11 +24,10 @@ class Text extends ConditionalFieldsHandlerBase {
     $state = [];
     // Text fields values are keyed by cardinality, so we have to flatten them.
     // TODO: support multiple values.
-    if ($options['values_set'] == CONDITIONAL_FIELDS_DEPENDENCY_VALUES_WIDGET) {
-      // Cast as array to handle the exception of autocomplete text fields.
-      $_info = $field_info['array_parents'][0];
-      $value = (array) $options[$_info];
-      $state[$options['state']][$options['selector']] = array_shift($value);
+    if ($options['values_set'] == CONDITIONAL_FIELDS_DEPENDENCY_VALUES_WIDGET && !empty($options['value_form'][0]['value'])) {
+      // TODO: Support autocommit.
+      $value = $options['value_form'][0]['value'];
+      $state[$options['state']][$options['selector']] = ['value' => $value];
     }
     return $state;
   }
