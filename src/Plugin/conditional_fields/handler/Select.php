271a4635006bbb0697e92494310f148ab48c84aa
Pavlo Samchuk <psamchuk@adyax.com>
2017-04-13 16:06:57 +0300

    Create a single select plugin.

--- src/Plugin/conditional_fields/handler/Select.php
+++ src/Plugin/conditional_fields/handler/Select.php
@@ -13,6 +13,11 @@ use Drupal\conditional_fields\ConditionalFieldsHandlerBase;
  */
 class Select extends ConditionalFieldsHandlerBase {
 
+  /**
+   * Define field settings to apply proper plugin.
+   *
+   * @var array
+   */
   protected $handler_conditions = [
     '#type' => 'select',
     '#multiple' => FALSE,
@@ -21,6 +26,41 @@ class Select extends ConditionalFieldsHandlerBase {
   /**
    * {@inheritdoc}
    */
-  public function statesHandler($field, $field_info, $options, &$state) { }
+  public function statesHandler($field, $field_info, $options, &$state) {
+    switch ($options['values_set']) {
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_WIDGET:
+        if (count($options['value_form']) == 1) {
+          $state[$options['state']][$options['selector']] = array('value' => $options['value_form'][0]['value']);
+        }
+        return;
+
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_AND:
+        // This input mode is not available for single select.
+        return;
+
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_XOR:
+        $state = [];
+        $state[$options['state']][] = 'xor';
+        break;
+
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_REGEX:
+        $regex = TRUE;
+        break;
+
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_NOT:
+      case CONDITIONAL_FIELDS_DEPENDENCY_VALUES_OR:
+        $state = [];
+        break;
+    }
+
+    $values = explode("\r\n", $options['values']);
+    foreach ($values as $value) {
+      $state[$options['state']][] = [
+        $options['selector'] => [
+          $options['condition'] => empty($regex) ? $value : ['regex' => $options['regex']],
+        ],
+      ];
+    }
+  }
 
 }
