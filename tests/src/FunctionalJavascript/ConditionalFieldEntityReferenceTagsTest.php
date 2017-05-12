9f6d3e1ac0f6c373feea34c7784df9664d83e91f
id.medion <psamchuk@adyax.com>
2017-05-12 16:08:14 +0300

    Refactoring ConditionalFieldEntityReferenceTagsTest.php

--- tests/src/FunctionalJavascript/ConditionalFieldEntityReferenceTagsTest.php
+++ tests/src/FunctionalJavascript/ConditionalFieldEntityReferenceTagsTest.php
@@ -115,9 +115,6 @@ class ConditionalFieldEntityReferenceTagsTest extends ConditionalFieldTestBase {
       '[name="grouping"]' => 'AND',
       '[name="state"]' => 'visible',
       '[name="effect"]' => 'show',
-      '[name="element_edit[1]"]' => 1,
-      '[name="element_view[1]"]' => 1,
-      '[name="element_view[2]"]' => 2,
     ];
     foreach ($data as $selector => $value) {
       $this->changeField($selector, $value);
